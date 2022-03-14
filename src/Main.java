ea60ea053fb85d2b9998d4cc3ca72b420baa3122
saddenly <37728443+onlytenders@users.noreply.github.com>
2022-03-14 20:27:29 +0600

    practice

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,30 @@
+import java.util.function.*;
+
 public class Main {
     public static void main(String[] args) {
-        Box<String, Banana> bananaBox = new Box<>("banana", new Banana());
-        bananaBox.getObj().printClass();
+        Predicate<Integer> isEven = x -> x%2==0;
+        System.out.println(isEven.test(4));
 
-        Box<String, Apple> appleBox = new Box<>("apple", new Apple());
-        appleBox.getObj().printClass();
+        Consumer<Integer> doubler = x -> System.out.println(x*2);
+        doubler.accept(234);
 
-        Box<String, Fruit> fruitBox = new Box<>("fruit", new Fruit());
-        fruitBox.getObj().printClass();
+        Function<Float, Integer> toInt = Math::round;
+        System.out.println(toInt.apply(4.5f));
+
+        Supplier<String> lorem = () -> "lorem ipsum dollor sit amet";
+        System.out.println(lorem.get());
+
+        UnaryOperator<Integer> factorOfTwo = x -> {
+            int result = 2;
+            for (int i = 1; i < x; i++) {
+                result *= 2;
+            }
+            return result;
+        };
+        System.out.println(factorOfTwo.apply(10));
+
+        BinaryOperator<String> joinStrings = (line1, line2) -> line1 + line2;
+
+        System.out.println(joinStrings.apply("some string ", "joined"));
     }
 }
