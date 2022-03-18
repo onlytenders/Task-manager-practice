352c240d1ba5bc83a11cb60ef586fe046b9ad9a5
saddenly <37728443+onlytenders@users.noreply.github.com>
2022-03-18 19:36:12 +0600

    practice

--- src/Main.java
+++ src/Main.java
@@ -2,29 +2,12 @@ import java.util.function.*;
 
 public class Main {
     public static void main(String[] args) {
-        Predicate<Integer> isEven = x -> x%2==0;
-        System.out.println(isEven.test(4));
+        Calculator calc = Calculator.instance.get();
 
-        Consumer<Integer> doubler = x -> System.out.println(x*2);
-        doubler.accept(234);
+        int a = calc.plus.apply(1, 1);
+        int b = calc.minus.apply(2, 1);
+        int c = calc.divide.apply(a, b);
 
-        Function<Float, Integer> toInt = Math::round;
-        System.out.println(toInt.apply(4.5f));
-
-        Supplier<String> lorem = () -> "lorem ipsum dollor sit amet";
-        System.out.println(lorem.get());
-
-        UnaryOperator<Integer> factorOfTwo = x -> {
-            int result = 2;
-            for (int i = 1; i < x; i++) {
-                result *= 2;
-            }
-            return result;
-        };
-        System.out.println(factorOfTwo.apply(10));
-
-        BinaryOperator<String> joinStrings = (line1, line2) -> line1 + line2;
-
-        System.out.println(joinStrings.apply("some string ", "joined"));
+        calc.println.accept(c);
     }
 }
